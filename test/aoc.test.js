75bd1f8544e3d2bb04cc4a17402d2b0c24483e1d
Tiark Rompf <tiark.rompf@gmail.com>
2023-12-03 00:01:37 -0500

    aoc day 1

--- test/aoc.test.js
+++ test/aoc.test.js
@@ -1,6 +1,39 @@
 const { api, pipe } = require('../src/rhyme')
 const { rh } = require('../src/parser')
 
+// 2023
+
+test("day1", () => {
+    let input = `1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet`
+  // utilities to split input
+  let udf = {
+    splitN: x => x.split("\n"),
+    splitB: x => x.split(""),
+    toNum: x => { 
+      let n = Number(x)
+      if (Number.isNaN(n)) 
+        return undefined 
+      else
+        return n
+    }
+  }
+  let root = {xxpath:"raw", xxparam: "inp"} // XXX
+  let digits =
+    pipe(root).get("input")
+    .map("udf.splitN").get("*line")
+    .map("udf.splitB").get("*char")
+    .map("udf.toNum")
+  let numbers = 
+    api.plus(api.times(api.first(digits), 10),  api.last(digits))
+  let query = 
+    pipe(numbers).group("*line").get("*").sum()
+  let func = api.compile(query)
+  let res = func({input, udf})
+  expect(res).toBe(142)
+})
 
 // 2022
 
@@ -38,8 +71,7 @@ test("day1", () => {
     .sum()
     .group("*chunk").get("*")
     .max()
-  let func = api.query(query)
+  let func = api.compile(query)
   let res = func({input, udf})
-  console.dir(res)
   expect(res).toBe(24000)
 })
