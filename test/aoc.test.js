3ea58b6c6594a78d5a99074e9edc4c394d1149df
guo543 <60993229+guo543@users.noreply.github.com>
2024-02-02 16:29:50 -0500

    AOC day4 part1 (#11)
    * add aoc day4 part1 and udf_stdlib funcs
    
    * minor change
    
    * use regex to match numbers in lines
    
    * also split the input by '|'
    
    * add more comment

--- test/aoc.test.js
+++ test/aoc.test.js
@@ -17,6 +17,9 @@ let udf_stdlib = {
   split: d => s => s.split(d),
   toNum: x => (n => Number.isNaN(n) ? undefined : n)(Number(x)),
   isLessOrEqual: (x,y) => x <= y,
+  isEqual: (x,y) => x === y,
+  exp: n => x => n ** x,
+  floor: x => Math.floor(x)
 }
 
 
@@ -200,6 +203,47 @@ let query = partNum.group("*match").group("*row").get("*0").get("*1").sum()
 let func = api.compile(query)
 let res = func({input, udf})
 expect(res).toBe(4361)
+})
+
+test("day4-part1", () => {
+  let input = `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`
+
+  let udf = {
+    getNums: s => s.match(/(\d+)/g),
+    ...udf_stdlib
+  }
+  
+  let line = rh`.input | udf.split "\\n" | .*line
+                       | udf.split ":" | .1
+                       | udf.split "|"`
+
+  let winNumber = rh`${line}.0 | udf.getNums | .*winNum`
+  let numberYouHave = rh`${line}.1 | udf.getNums | .*numYouHave`
+
+  let number = rh`${api.array(winNumber, numberYouHave)} | .*num`
+
+  // "count number | group number" groups the count of each number by number
+  // which gives us the frequencies of numbers in an object
+  
+  // We then to look for numbers with frequency = 2
+  // with the underlying assumption that
+  // each winning number and each number you have is unique
+  let matchCount = rh`count ${number} | group ${number}
+                                      | udf.isEqual .*freq 2
+                                      | sum`
+
+  let lineRes = rh`${matchCount} - 1 | udf.exp 2 | udf.floor`
+
+  let query = rh`${lineRes} | group *line | sum .*`
+
+  let func = api.compile(query)
+  let res = func({input, udf})
+  expect(res).toBe(13)
 })
 
 // 2022
