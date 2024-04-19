e6436ad517e4e32f2bcf34d34566bd36e22f1039
Tiark Rompf <tiark.rompf@gmail.com>
2024-04-19 19:48:24 -0400

    encoded versions of aggregateAsKey

--- test/semantics/se-encoding.test.js
+++ test/semantics/se-encoding.test.js
@@ -63,3 +63,59 @@ test("group_encoded2", () => {
     V: 10/70,
   })
 })
+
+
+// ----- aggregateAsKey
+
+test("aggregateAsKey_encoded1", () => {
+
+  let data = [
+      {"A": 1, "B": 10},
+      {"A": 2, "B": 20},
+      {"A": 1, "B": 30},
+  ]
+
+  // let q1 = { "data.*.A": { "sum(data.*.B)": true } }
+
+  let query = {"*K1": {"*K2": 
+    rh`mkset(sum(mkset(data.*.A).*K1 & data.*.B)).*K2 & true`
+  }}
+
+  // this one is not correct:
+  // rh`mkset(data.*.A).*K1 & mkset(sum(data.*.B)).*K2 & true`
+
+  let func = compile(query)
+  let res1 = func({data})
+
+  let e1 = {
+      1: { 40: "true" },
+      2: { 20: "true" }
+  }
+
+  expect(res1).toEqual(e1)
+})
+
+test("aggregateAsKey_encoded2", () => {
+
+  let data = [
+      {"A": 1, "B": 10},
+      {"A": 2, "B": 20},
+      {"A": 1, "B": 30},
+  ]
+
+  // let q1 = { "sum(data.*.B)": { "data.*.A": true } }
+
+  let query = {"*K1": {"*K2": 
+    rh`mkset(sum(data.*.B)).*K1 & mkset(data.*.A).*K2 & true`
+  }}
+
+  let func = compile(query)
+  let res2 = func({data})
+
+  let e2_alt_string = {
+      60: { 1: "true", 2: "true" } // XXX is this the right one?
+  }
+
+  expect(res2).toEqual(e2_alt_string)
+})
+
