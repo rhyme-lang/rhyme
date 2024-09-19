469982a04b16529127733bf71d9a5bc953704f48
Tiark Rompf <tiark.rompf@gmail.com>
2024-09-19 18:45:23 -0400

    test behavior for empty slots
    empty slots can occur anywhere, due to elements being
    filtered out or due to larger than necessary size of
    the provided array
    
    unclear if this is problematic -- it seems reasonable
    if written explicitly by the programmer, more questionable
    if choice of storage is inferred implicitly

--- test/semantics/se-datastruct.test.js
+++ test/semantics/se-datastruct.test.js
@@ -8,6 +8,7 @@ const { compile } = require('../../src/simple-eval')
 // a generic object.
 
 // first: arrays
+// note: there may be empty/undefined slots
 
 test("array0", () => {
 
@@ -22,5 +23,67 @@ test("array0", () => {
 
   expect(res).toEqual(expected)
   expect(res).not.toEqual({...expected}) // not an object!
+})
+
+
+test("array1_largeInitialSize", () => {
+
+  let data = [1,2,3,4,5]
+  let udf = { array: x => new Array(x) }
+  let query = rh`update (udf.array 7) *D (data.*D + 10)`
+
+  let func = compile(query)
+  let res = func({data, udf})
+
+  // non-strict equality
+
+  expect(res).not.toEqual({
+  	0:11,1:12,2:13,3:14,4:15
+  })
+
+  expect(res).toEqual([
+  	11,12,13,14,15
+  ])
+
+  expect(res).toEqual([
+  	11,12,13,14,15, undefined, undefined, undefined,
+  ])
+
+
+  // strict equality
+
+  expect(res).not.toStrictEqual([
+  	11,12,13,14,15
+  ])
+
+  expect(res).not.toStrictEqual([
+  	11,12,13,14,15, undefined, undefined, undefined,
+  ])
+
+  expect(res).not.toStrictEqual([
+  	11,12,13,14,15, undefined, undefined, // not even this is equal!
+  ])
+
+  expect(res).toStrictEqual([
+  	11,12,13,14,15, , , 
+  ])
+})
+
+test("array2_filterGaps", () => {
+  let data = [1,2,3,4,5]
+  let filter = { 1: true, 3: true, 5: true }
+  let udf = { array: x => new Array(x) }
+  let query = rh`update (udf.array 5) *D (filter.(data.*D) & data.*D + 10)`
+
+  let func = compile(query)
+  let res = func({data, filter, udf})
+
+  expect(res).toEqual([
+  	11,,13,,15,
+  ])
+
+  expect(res).not.toEqual([
+  	11,12,13,14,15,
+  ])
 })
 
