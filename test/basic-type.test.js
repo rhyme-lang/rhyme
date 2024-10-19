6ab9032373cb9c8e3c20ec592c8d813756cccedf
Ran Guo <60993229+guo543@users.noreply.github.com>
2024-10-18 21:45:25 -0400

    integrated updated type system and cgen for sql (#52)
    * support zip and join
    
    * add basic stateful print and complete common stateful ops
    
    * Update type system for C and update object structure (#51)
    
    * integrate new type system and csql codegen
    
    * remove debug prints
    
    * bugfix
    
    ---------
    
    Co-authored-by: IsaacFleetwood <47401661+IsaacFleetwood@users.noreply.github.com>

--- test/basic-type.test.js
+++ test/basic-type.test.js
@@ -1,158 +1,51 @@
 const { api } = require('../src/rhyme')
 const { rh } = require('../src/parser')
-const { typing } = require("../src/typing");
-
-const { runtime } = require('../src/simple-runtime')
-let rt = runtime
-
-
+const { typing, types, prettyPrintType } = require("../src/typing");
 
 // some sample data for testing
 let data = [
     { key: "A", value: 10 },
     { key: "B", value: 20 },
     { key: "A", value: 30 }
-]
+];
 
-let sym = Symbol("*")
-let dataSchema = {
-    [Symbol("*A")]: {
-        key: typing.string,
-        value: typing.number
-    }
-};
-let otherSchema = {
-    [Symbol("*B")]: {
-        key: typing.string,
-        value: typing.number
-    }
-};
+let key = typing.createKey(types.u8);
+let key2 = typing.createKey(types.u8);
+
+let dataSchema = typing.objBuilder()
+    .add(key, typing.createSimpleObject({
+        key: typing.createUnion("A", "B"),
+        value: types.u8
+    }))
+    .build();
+
+let otherSchema = typing.objBuilder()
+    .add(key, typing.createSimpleObject({
+        key: typing.createUnion("A", "B"),
+        value: types.u8
+    }))
+    .build();
+
+test("type-creation-1", () => {
+    let type = typing.createUnion(types.u8, types.u16);
+    expect(type).toBe(types.u16);
+})
+
+test("type-creation-2", () => {
+    let type = typing.createUnion(types.u8, types.i16);
+    expect(type).toBe(types.i16);
+})
+
+test("type-creation-3", () => {
+    let func = api.compile(api.plus(1, -1), types.nothing);
+    expect(func.explain2.resultType).toBe(types.i16);
+})
 
 test("plainSumTest", () => {
-    let query = api.sum("data.*.value");
-    let func = api.compile(query, {item: typing.number, data: dataSchema, other: otherSchema});
-    
-    // console.log(func.c2.explain.ir.filters[0])
-    // console.log(func.c2.explain.pseudo)
-    // in the output, type of data[D0] is shown as {}?
-
-    let res = func({ data, other: data })
-    let expected = 60
-    expect(res).toBe(expected)
+    let query = {"data.*A.key": api.sum("other.*A.value")};
+    let func = api.compile(query, typing.createSimpleObject({data: dataSchema, other: otherSchema}));
+    //console.log(prettyPrintType(func.explain2.resultType));
+    //console.log(prettyPrintType(typing.objBuilder().add(typing.createUnion("A", "B"), types.u8).build()));
+    //expect(typing.isSubtype(typing.objBuilder().add(typing.createUnion("A", "B"), types.u8).build(), func.explain2.resultType)).toBe(true);
+    //expect(res).toBe(expected)
 })
-
-test("plusTest1a", () => {
-    let query = rh`2 + .arg`
-    let func = api.compile(query, {arg: typing.number})
-    
-    let res = func({ arg: 8 })
-    let expected = 10
-    expect(res).toBe(expected)
-})
-
-test("plusTest1b", () => {
-    let query = rh`2 + .arg`
-    expect(() => {
-        let func = api.compile(query, {arg: typing.string})
-        
-        let res = func({ arg: "HELLO" })
-        let expected = 10
-        expect(res).toBe(expected)
-    }).toThrow(new Error("Unable to conform arg type of String to (Number | Nothing)"))
-})
-
-test("plusTest2a", () => {
-    let query = rh`2 + .arg.field`
-    let func = api.compile(query, {arg: {field: typing.number}})
-    
-    let res = func({ arg: { field: 8 }})
-    let expected = 10
-    expect(res).toBe(expected)
-})
-
-test("plusTest2b", () => {
-    let query = rh`2 + .arg.field`
-    expect(() => {
-        let func = api.compile(query, {arg: {field: typing.string}})
-    
-        let res = func({ arg: { field: "HELLO" }})
-        let expected = 10
-        expect(res).toBe(expected)
-    }).toThrow(new Error("Unable to conform arg type of String to (Number | Nothing)"))
-})
-
-test("plusTest2c", () => {
-    let query = rh`2 + .arg.field`
-    expect(() => {
-        let func = api.compile(query, {arg: typing.string})
-    
-        let res = func({ arg: "HELLO" })
-        let expected = 10
-        expect(res).toBe(expected)
-    }).toThrow(new Error("Error in attempting to access field on type: String"))
-})
-
-test("plusTest2d", () => {
-    let query = rh`2 + .arg.field`
-    // expect(() => {
-        let func = api.compile(query, {arg: {field1: typing.number}})
-    
-        let res = func.c2({ arg: { field1: 8 }})
-        let expected = 10
-        expect(res).toBe(undefined)
-    // TBD: what is the expected behavior? type error?
-    // }).toThrow(new Error("Error in attempting to access field on type: String"))
-})
-
-test("plusTest2e", () => {
-    let query = rh`2 + .arg.field`
-    // expect(() => {
-        let func = api.compile(query, {arg1: {field: typing.number}})
-    
-        let res = func.c2({ arg1: { field: 8 }})
-        let expected = 10
-        expect(res).toBe(undefined)
-    // TBD: what is the expected behavior? type error?
-    // }).toThrow(new Error("Error in attempting to access field on type: String"))
-})
-
-test("plusTest3a", () => {
-    let query = rh`2 + .arg.*`
-    let func = api.compile(query, {arg: {field: typing.number}})
-    
-    let res = func.c2({ arg: { field: 8 }})
-    let expected = {field: 10}
-    expect(res).toEqual(expected)
-})
-
-test("plusTest3b", () => {
-    let query = rh`2 + .arg.*`
-    expect(() => {
-        let func = api.compile(query, {arg: {field: typing.string}})
-        
-        let res = func.c2({ arg: { field: 8 }})
-        let expected = {field: 10}
-        expect(res).toEqual(expected)
-    }).toThrow(new Error("Unable to conform arg type of String to (Number | Nothing)"))
-})
-
-test("plusTest4a", () => {
-    let query = rh`2 + .*.field`
-    let func = api.compile(query, {arg: {field: typing.number}})
-    
-    let res = func.c2({ arg: { field: 8 }})
-    let expected = {arg: 10}
-    expect(res).toEqual(expected)
-})
-
-test("plusTest4b", () => {
-    let query = rh`2 + .*.field`
-    expect(() => {
-        let func = api.compile(query, {arg: {field: typing.string}})
-        
-        let res = func.c2({ arg: { field: 8 }})
-        let expected = {arg: 10}
-        expect(res).toEqual(expected)
-    }).toThrow(new Error("Unable to conform arg type of String to (Number | Nothing)"))
-})
-
--- test/semantics/se-cgen-sql.test.js
+++ test/semantics/se-cgen-sql.test.js
@@ -2,7 +2,7 @@ const { api, pipe } = require('../../src/rhyme')
 const { rh, parse } = require('../../src/parser')
 const { compile } = require('../../src/simple-eval')
 const { preproc } = require('../../src/preprocess')
-const { typing } = require('../../src/typing')
+const { typing, types } = require('../../src/typing')
 
 const fs = require('node:fs/promises')
 const os = require('node:child_process')
@@ -44,21 +44,21 @@ test("testTrivial", async () => {
   expect(res).toEqual("201\n")
 })
 
-let schema = {
-  [Symbol("*A")]: {
-    A: typing.string,
-    B: typing.number,
-    C: typing.number,
-    D: typing.number
-  }
-}
+let schema = typing.objBuilder()
+  .add(typing.createKey(types.u32), typing.createSimpleObject({
+    A: types.string,
+    B: types.i32,
+    C: types.i32,
+    D: types.i32
+  })).build()
+
 
 test("testSimpleSum1", async () => {
   let csv = rh`loadCSV "./cgen-sql/simple.csv" ${schema}`
 
   let query = rh`${csv}.*.C | sum`
 
-  let func = compile(query, { backend: "c-sql", schema: typing.nothing })
+  let func = compile(query, { backend: "c-sql", schema: types.nothing })
   
   let res = await func()
   expect(res).toEqual("228\n")
@@ -69,7 +69,7 @@ test("testSimpleSum2", async () => {
 
   let query = rh`${csv}.*.C + 10 | sum`
 
-  let func = compile(query, { backend: "c-sql", schema: typing.nothing })
+  let func = compile(query, { backend: "c-sql", schema: types.nothing })
 
   let res = await func()
   expect(res).toEqual("268\n")
@@ -80,7 +80,7 @@ test("testSimpleSum3", async () => {
 
   let query = rh`(${csv}.*.C | sum) + 10`
 
-  let func = compile(query, { backend: "c-sql", schema: typing.nothing })
+  let func = compile(query, { backend: "c-sql", schema: types.nothing })
 
   let res = await func()
   expect(res).toEqual("238\n")
@@ -91,7 +91,7 @@ test("testSimpleSum4", async () => {
 
   let query = rh`sum(${csv}.*A.C) + sum(${csv}.*B.D)`
 
-  let func = compile(query, { backend: "c-sql", schema: typing.nothing })
+  let func = compile(query, { backend: "c-sql", schema: types.nothing })
 
   let res = await func()
   expect(res).toEqual("243\n")
@@ -100,23 +100,93 @@ test("testSimpleSum4", async () => {
 test("testSimpleSum5", async () => {
   let csv = rh`loadCSV "./cgen-sql/simple.csv" ${schema}`
 
-  let query = rh`sum(${csv}.*A.C) + sum(${csv}.*A.D)`
+  let query = rh`sum(${csv}.*A.C + ${csv}.*A.D)`
 
-  let func = compile(query, { backend: "c-sql", schema: typing.nothing })
+  let func = compile(query, { backend: "c-sql", schema: types.nothing })
 
   let res = await func()
   expect(res).toEqual("243\n")
 })
 
-test("testLoadCSVMultipleFiles", async () => {
+test("testSimpleSum6", async () => {
+  let csv = rh`loadCSV "./cgen-sql/simple.csv" ${schema}`
+
+  let query = rh`sum(${csv}.*A.C + ${csv}.*A.D)`
+
+  let func = compile(query, { backend: "c-sql", schema: types.nothing })
+
+  let res = await func()
+  expect(res).toEqual("243\n")
+})
+
+test("testLoadCSVMultipleFilesZip", async () => {
   let csv1 = rh`loadCSV "./cgen-sql/simple.csv" ${schema}`
   let csv2 = rh`loadCSV "./cgen-sql/simple_copy.csv" ${schema}`
 
-  let query = rh`sum(${csv1}.*A.C) + sum(${csv2}.*B.D)`
+  let query = rh`sum(${csv1}.*A.C + ${csv2}.*A.D)`
 
-  let func = compile(query, { backend: "c-sql", schema: typing.nothing })
+  let func = compile(query, { backend: "c-sql", schema: types.nothing })
 
   let res = await func()
   expect(res).toEqual("231\n")
 })
 
+test("testLoadCSVMultipleFilesJoin", async () => {
+  let csv1 = rh`loadCSV "./cgen-sql/simple.csv" ${schema}`
+  let csv2 = rh`loadCSV "./cgen-sql/simple_copy.csv" ${schema}`
+
+  let query = rh`sum(${csv1}.*A.C + ${csv2}.*B.D)`
+
+  let func = compile(query, { backend: "c-sql", schema: types.nothing })
+
+  let res = await func()
+  expect(res).toEqual("924\n")
+})
+
+test("testMin", async () => {
+  let csv = rh`loadCSV "./cgen-sql/simple.csv" ${schema}`
+
+  let query = rh`min ${csv}.*.B`
+
+  let func = compile(query, { backend: "c-sql", schema: types.nothing })
+
+  let res = await func()
+  expect(res).toEqual("1\n")
+})
+
+test("testMax", async () => {
+  let csv = rh`loadCSV "./cgen-sql/simple.csv" ${schema}`
+
+  let query = rh`max ${csv}.*.C`
+
+  let func = compile(query, { backend: "c-sql", schema: types.nothing })
+
+  let res = await func()
+  expect(res).toEqual("123\n")
+})
+
+test("testCount", async () => {
+  let csv = rh`loadCSV "./cgen-sql/simple.csv" ${schema}`
+
+  let query = rh`count ${csv}.*.C`
+
+  let func = compile(query, { backend: "c-sql", schema: types.nothing })
+
+  let res = await func()
+  expect(res).toEqual("4\n")
+})
+
+test("testStatefulPrint", async () => {
+  let csv = rh`loadCSV "./cgen-sql/simple.csv" ${schema}`
+
+  let query = rh`print ${csv}.*.B`
+
+  let func = compile(query, { backend: "c-sql", schema: types.nothing })
+
+  let res = await func()
+  expect(res).toEqual(`5
+2
+1
+7
+0\n`)
+})
