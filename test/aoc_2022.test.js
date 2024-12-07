56dbd534773392de8551d3517f15a16a3851f7f9
Tiark Rompf <tiark.rompf@gmail.com>
2024-12-07 13:29:03 -0500

    extract aoc 2022 tests into their own file

---
+++ test/aoc_2022.test.js
@@ -0,0 +1,123 @@
+/*
+ * Implementations of (some) "Advent of Code" challenges 2022
+ *
+ * Problem statements: https://adventofcode.com
+ *
+ * Solutions are inspired by existing solutions in:
+ * - JQ: https://github.com/odnoletkov/advent-of-code-jq
+ * - Scala: https://scalacenter.github.io/scala-advent-of-code
+ *
+ */
+
+const { api, pipe } = require('../src/rhyme')
+const { rh } = require('../src/parser')
+const { compile } = require('../src/simple-eval')
+
+
+// 2022
+
+test("day1-A", () => {
+    let input = `1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`
+  // utilities to split input
+  let udf = {
+    splitNN: x => x.split("\n\n"),
+    splitN: x => x.split("\n"),
+    toNum: x => Number(x),
+  }
+  // standard api:
+  let query = api.max(api.get({
+    "*chunk": api.sum(api.apply("udf.toNum",
+        api.get(api.apply("udf.splitN",
+          api.get(api.apply("udf.splitNN",
+            ".input"),
+          "*chunk")),
+        "*line")))
+  },"*"))
+  let func = api.compile(query)
+  let res = func({input, udf})
+  expect(res).toBe(24000)
+})
+
+test("day1-B", () => {
+    let input = `1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`
+  // utilities to split input
+  let udf = {
+    splitNN: x => x.split("\n\n"),
+    splitN: x => x.split("\n"),
+    toNum: x => Number(x),
+  }
+  // potential textual syntax:
+  // input | split "\n\n"  | get *chunk
+  //       | split "\n"    | get *line  | toNum
+  //       | sum | group *chunk | get * | max
+  let query =
+    pipe("input")
+    .map("udf.splitNN").get("*chunk")
+    .map("udf.splitN").get("*line")
+    .map("udf.toNum")
+    .sum()
+    .group("*chunk").get("*")
+    .max()
+  let func = api.compile(query)
+  let res = func({input, udf})
+  expect(res).toBe(24000)
+})
+
+test("day1-C", () => {
+    let input = `1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`
+  // utilities to split input
+  let udf = {
+    splitNN: x => x.split("\n\n"),
+    splitN: x => x.split("\n"),
+    toNum: x => Number(x),
+  }
+  // potential textual syntax:
+  // input | split "\n\n"  | get *chunk
+  //       | split "\n"    | get *line  | toNum
+  //       | sum | group *chunk | get * | max
+  let query = rh`.input | udf.splitNN | .*chunk
+                        | udf.splitN  | .*line | udf.toNum
+                        | sum | group *chunk | .* | max`
+  let func = api.compile(query)
+  let res = func({input, udf})
+  expect(res).toBe(24000)
+})
--- test/aoc_2023.test.js
+++ test/aoc_2023.test.js
@@ -1,5 +1,5 @@
 /*
- * Implementations of "Advent of Code" challenges
+ * Implementations of "Advent of Code" challenges 2023
  *
  * Problem statements: https://adventofcode.com
  *
@@ -42,8 +42,6 @@ let udf_stdlib = {
   ifThenElse: (predicate, thenBr, elseBr) => predicate ? thenBr : elseBr,
 }
 
-
-// 2023
 
 test("day1-part1", () => {
   let input = `1abc2
@@ -3170,110 +3168,3 @@ test("day24-part1", () => {
   expect(res).toBe(2)
 })
 
-// 2022
-
-test("day1-A", () => {
-    let input = `1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000`
-  // utilities to split input
-  let udf = {
-    splitNN: x => x.split("\n\n"),
-    splitN: x => x.split("\n"),
-    toNum: x => Number(x),
-  }
-  // standard api:
-  let query = api.max(api.get({
-    "*chunk": api.sum(api.apply("udf.toNum",
-        api.get(api.apply("udf.splitN",
-          api.get(api.apply("udf.splitNN",
-            ".input"),
-          "*chunk")),
-        "*line")))
-  },"*"))
-  let func = api.compile(query)
-  let res = func({input, udf})
-  expect(res).toBe(24000)
-})
-
-test("day1-B", () => {
-    let input = `1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000`
-  // utilities to split input
-  let udf = {
-    splitNN: x => x.split("\n\n"),
-    splitN: x => x.split("\n"),
-    toNum: x => Number(x),
-  }
-  // potential textual syntax:
-  // input | split "\n\n"  | get *chunk
-  //       | split "\n"    | get *line  | toNum
-  //       | sum | group *chunk | get * | max
-  let query =
-    pipe("input")
-    .map("udf.splitNN").get("*chunk")
-    .map("udf.splitN").get("*line")
-    .map("udf.toNum")
-    .sum()
-    .group("*chunk").get("*")
-    .max()
-  let func = api.compile(query)
-  let res = func({input, udf})
-  expect(res).toBe(24000)
-})
-
-test("day1-C", () => {
-    let input = `1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000`
-  // utilities to split input
-  let udf = {
-    splitNN: x => x.split("\n\n"),
-    splitN: x => x.split("\n"),
-    toNum: x => Number(x),
-  }
-  // potential textual syntax:
-  // input | split "\n\n"  | get *chunk
-  //       | split "\n"    | get *line  | toNum
-  //       | sum | group *chunk | get * | max
-  let query = rh`.input | udf.splitNN | .*chunk
-                        | udf.splitN  | .*line | udf.toNum
-                        | sum | group *chunk | .* | max`
-  let func = api.compile(query)
-  let res = func({input, udf})
-  expect(res).toBe(24000)
-})
