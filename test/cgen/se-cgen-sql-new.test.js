5d0ef9ad952716c63e1bf64cd73ccfd54dc68120
guo543 <guo543@purdue.edu>
2025-01-21 15:16:46 -0500

    add hash join test

--- test/cgen/se-cgen-sql-new.test.js
+++ test/cgen/se-cgen-sql-new.test.js
@@ -531,4 +531,17 @@ test("groupByArray", async () => {
   console.log(res)
 })
 
+test("hashJoinArray", async () => {
+  let country = rh`loadCSV "./cgen-sql/country.csv" ${countrySchema}`
+  let region = rh`loadCSV "./cgen-sql/region.csv" ${regionSchema}`
+
+  let q1 = rh`${region}.*O.region | group ${region}.*O.country`
+  let query = rh`array ${country}.*.population | group ${q1}.(${country}.*.country)`
+
+  let func = await compile(query, { backend: "c-sql-new", outDir, outFile: "hashJoinArray.c", schema: types.never })
+  let res = await func()
+
+  console.log(res)
+})
+
 /**/
