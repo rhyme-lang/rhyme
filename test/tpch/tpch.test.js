9db1472a1f8cc600538455fd57dd4e0f8245bcc6
guo543 <guo543@purdue.edu>
2025-04-21 15:19:55 -0400

    add tpch q11

--- test/tpch/tpch.test.js
+++ test/tpch/tpch.test.js
@@ -8,6 +8,8 @@ const os = require('child_process')
 let dataDir = "cgen-sql/data/SF1"
 let outDir = "cgen-sql/out-tpch"
 
+let answersDir = "cgen-sql/tpch-answers"
+
 let customerSchema = typing.objBuilder()
   .add(typing.createKey(types.u32), typing.createSimpleObject({
     c_custkey: types.i32,
@@ -354,7 +356,6 @@ test("q8", async () => {
 
   let lineitem1 = rh`[{
     p_partkey: ${part1}.(${lineitem}.*l1.l_partkey).*p2,
-    l_orderkey: ${lineitem}.*l1.l_orderkey,
     l_suppkey: ${lineitem}.*l1.l_suppkey,
     l_extendedprice: ${lineitem}.*l1.l_extendedprice,
     l_discount: ${lineitem}.*l1.l_discount
@@ -364,7 +365,6 @@ test("q8", async () => {
     l_suppkey: ${lineitem1}.(${orders}.*o1.o_orderkey).*l2.l_suppkey,
     l_extendedprice: ${lineitem1}.(${orders}.*o1.o_orderkey).*l2.l_extendedprice,
     l_discount: ${lineitem1}.(${orders}.*o1.o_orderkey).*l2.l_discount,
-    o_custkey: ${orders}.*o1.o_custkey,
     o_orderdate: ${orders}.*o1.o_orderdate
   }] | group ${orders}.*o1.o_custkey`
 
@@ -377,7 +377,6 @@ test("q8", async () => {
   }] | group ${orders1}.(${customer}.*c1.c_custkey).*o2.l_suppkey`
 
   let nation2 = rh`[{
-    n_nationkey: ${nation}.*n2.n_nationkey,
     n_name: ${nation}.*n2.n_name
   }] | group ${nation}.*n2.n_nationkey`
 
@@ -399,7 +398,7 @@ test("q8", async () => {
   expect(res).toBe(`1995|0.0344|
 1996|0.0415|
 `)
-}, 10 * 1000)
+}, 15 * 1000)
 
 test("q10", async () => {
   let nation1 = rh`[${nation}.*n1.n_name] | group ${nation}.*n1.n_nationkey`
@@ -441,25 +440,37 @@ test("q10", async () => {
   let func = await compile(query, { backend: "c-sql-new", outDir, outFile: "q10", schema: types.never, limit: 20 })
   let res = await func()
 
-  expect(res).toBe(`57040|Customer#000057040|734235.2455|632.8700|JAPAN|Eioyzjf4pp|22-895-641-3466|sits. slyly regular requests sleep alongside of the regular inst|
-143347|Customer#000143347|721002.6948|2557.4700|EGYPT|1aReFYv,Kw4|14-742-935-3718|ggle carefully enticing requests. final deposits use bold, bold pinto beans. ironic, idle re|
-60838|Customer#000060838|679127.3077|2454.7700|BRAZIL|64EaJ5vMAHWJlBOxJklpNc2RJiWE|12-913-494-9813| need to boost against the slyly regular account|
-101998|Customer#000101998|637029.5667|3790.8900|UNITED KINGDOM|01c9CILnNtfOQYmZj|33-593-865-6378|ress foxes wake slyly after the bold excuses. ironic platelets are furiously carefully bold theodolites|
-125341|Customer#000125341|633508.0860|4983.5100|GERMANY|S29ODD6bceU8QSuuEJznkNaK|17-582-695-5962|arefully even depths. blithely even excuses sleep furiously. foxes use except the dependencies. ca|
-25501|Customer#000025501|620269.7849|7725.0400|ETHIOPIA|  W556MXuoiaYCCZamJI,Rn0B4ACUGdkQ8DZ|15-874-808-6793|he pending instructions wake carefully at the pinto beans. regular, final instructions along the slyly fina|
-115831|Customer#000115831|596423.8672|5098.1000|FRANCE|rFeBbEEyk dl ne7zV5fDrmiq1oK09wV7pxqCgIc|16-715-386-3788|l somas sleep. furiously final deposits wake blithely regular pinto b|
-84223|Customer#000084223|594998.0239|528.6500|UNITED KINGDOM|nAVZCs6BaWap rrM27N 2qBnzc5WBauxbA|33-442-824-8191| slyly final deposits haggle regular, pending dependencies. pending escapades wake |
-54289|Customer#000054289|585603.3918|5583.0200|IRAN|vXCxoCsU0Bad5JQI ,oobkZ|20-834-292-4707|ely special foxes are quickly finally ironic p|
-39922|Customer#000039922|584878.1134|7321.1100|GERMANY|Zgy4s50l2GKN4pLDPBU8m342gIw6R|17-147-757-8036|y final requests. furiously final foxes cajole blithely special platelets. f|
-6226|Customer#000006226|576783.7606|2230.0900|UNITED KINGDOM|8gPu8,NPGkfyQQ0hcIYUGPIBWc,ybP5g,|33-657-701-3391|ending platelets along the express deposits cajole carefully final |
-922|Customer#000000922|576767.5333|3869.2500|GERMANY|Az9RFaut7NkPnc5zSD2PwHgVwr4jRzq|17-945-916-9648|luffily fluffy deposits. packages c|
-147946|Customer#000147946|576455.1320|2030.1300|ALGERIA|iANyZHjqhyy7Ajah0pTrYyhJ|10-886-956-3143|ithely ironic deposits haggle blithely ironic requests. quickly regu|
-115640|Customer#000115640|569341.1933|6436.1000|ARGENTINA|Vtgfia9qI 7EpHgecU1X|11-411-543-4901|ost slyly along the patterns; pinto be|
-73606|Customer#000073606|568656.8578|1785.6700|JAPAN|xuR0Tro5yChDfOCrjkd2ol|22-437-653-6966|he furiously regular ideas. slowly|
-110246|Customer#000110246|566842.9815|7763.3500|VIETNAM|7KzflgX MDOq7sOkI|31-943-426-9837|egular deposits serve blithely above the fl|
-142549|Customer#000142549|563537.2368|5085.9900|INDONESIA|ChqEoK43OysjdHbtKCp6dKqjNyvvi9|19-955-562-2398|sleep pending courts. ironic deposits against the carefully unusual platelets cajole carefully express accounts.|
-146149|Customer#000146149|557254.9865|1791.5500|ROMANIA|s87fvzFQpU|29-744-164-6487| of the slyly silent accounts. quickly final accounts across the |
-52528|Customer#000052528|556397.3509|551.7900|ARGENTINA|NFztyTOR10UOJ|11-208-192-3205| deposits hinder. blithely pending asymptotes breach slyly regular re|
-23431|Customer#000023431|554269.5360|3381.8600|ROMANIA|HgiV0phqhaIa9aydNoIlb|29-915-458-2654|nusual, even instructions: furiously stealthy n|
-`)
+  let answer = fs.readFileSync(`${answersDir}/q10.out`).toString()
+
+  expect(res).toBe(answer)
+})
+
+test("q11", async () => {
+  let nation1 = rh`[${nation}.*n1.n_name == "GERMANY" & ${nation}.*n1.n_nationkey] | group ${nation}.*n1.n_nationkey`
+
+  let supplier1 = rh`[{
+    n_nationkey: ${nation1}.(${supplier}.*s1.s_nationkey).*n2,
+    s_suppkey: ${supplier}.*s1.s_suppkey
+  }] | group ${supplier}.*s1.s_suppkey`
+
+  // redundant condition, just to make *s2 a dependency for the sum
+  let cond = rh`${supplier1}.(${partsupp}.*ps1.ps_suppkey).*s2.s_suppkey == ${partsupp}.*ps1.ps_suppkey`
+  let sum = rh`(sum (${cond} & (${partsupp}.*ps1.ps_supplycost * ${partsupp}.*ps1.ps_availqty))) * 0.0001`
+
+  let partsupp1 = rh`sum (${cond} & (${partsupp}.*ps1.ps_supplycost * ${partsupp}.*ps1.ps_availqty)) | group ${partsupp}.*ps1.ps_partkey`
+
+  let cond2 = rh`${partsupp1}.(${partsupp}.*ps2.ps_partkey) > 0.0 && ${partsupp1}.(${partsupp}.*ps2.ps_partkey) > ${sum}`
+  let partsupp2 = rh`{
+    ps_partkey: ${cond2} & ${partsupp}.*ps2.ps_partkey,
+    value: ${cond2} & ${partsupp1}.(${partsupp}.*ps2.ps_partkey)
+  } | group ${partsupp}.*ps2.ps_partkey`
+
+  let query = rh`sort "value" 1 ${partsupp2}`
+
+  let func = await compile(query, { backend: "c-sql-new", outDir, outFile: "q11", schema: types.never })
+  let res = await func()
+
+  let answer = fs.readFileSync(`${answersDir}/q11.out`).toString()
+
+  expect(res).toBe(answer)
 })
