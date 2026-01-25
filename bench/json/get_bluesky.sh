261ff68fbb7d676bcd39edc7bc6c80f02467ab0a
guo543 <guo543@purdue.edu>
2026-01-25 14:47:41 -0800

    add data cleanup

--- bench/json/get_bluesky.sh
+++ bench/json/get_bluesky.sh
@@ -8,10 +8,26 @@ zcat $DATA_DIR/file_0001.json.gz \
      $DATA_DIR/file_0002.json.gz \
      $DATA_DIR/file_0003.json.gz \
      $DATA_DIR/file_0004.json.gz \
-     $DATA_DIR/file_0005.json.gz \
-     $DATA_DIR/file_0006.json.gz \
-     $DATA_DIR/file_0007.json.gz \
-     $DATA_DIR/file_0008.json.gz \
+     > $DATA_DIR/bluesky.json
+
+zcat $DATA_DIR/file_0005.json.gz > $DATA_DIR/file_0005.json
+sed -i '91840{N;s/\n//}' $DATA_DIR/file_0005.json
+cat $DATA_DIR/file_0005.json >> $DATA_DIR/bluesky.json
+# rm $DATA_DIR/file_0005.json
+
+zcat $DATA_DIR/file_0006.json.gz > $DATA_DIR/file_0006.json
+sed -i '17282{N;s/\n//}' $DATA_DIR/file_0006.json
+cat $DATA_DIR/file_0006.json >> $DATA_DIR/bluesky.json
+# rm $DATA_DIR/file_0006.json
+
+zcat $DATA_DIR/file_0007.json.gz > $DATA_DIR/file_0007.json
+sed -i '752367{N;s/\n//}' $DATA_DIR/file_0007.json
+cat $DATA_DIR/file_0007.json >> $DATA_DIR/bluesky.json
+# rm $DATA_DIR/file_0007.json
+
+zcat $DATA_DIR/file_0008.json.gz \
      $DATA_DIR/file_0009.json.gz \
      $DATA_DIR/file_0010.json.gz \
-     > $DATA_DIR/bluesky.json
+     >> $DATA_DIR/bluesky.json
+
+sed -i 's/\\u0000//g' $DATA_DIR/bluesky.json
